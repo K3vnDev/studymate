60b997cbf79459b701eb31d994b55bfbe88e920d
K3vnDev <158790560+K3vnDev@users.noreply.github.com>
2024-12-23 18:19:38 -0500

    Enchance Sidebar design and responsiveness

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,50 +1,77 @@
 import { FONTS } from '@/consts'
 import Link from 'next/link'
-import { AppIcon, ClockIcon, HomeIcon, MessageIcon, ProfileIcon } from './icons'
+import { useEffect, useState } from 'react'
+import { AppIcon, BookIcon, ClockIcon, HomeIcon, MessageIcon, ProfileIcon } from './icons'
 
 export const Sidebar = () => {
-  const paths = [
-    {
-      name: 'Dashboard',
-      icon: <HomeIcon />
-    },
-    {
-      name: 'Focus',
-      icon: <ClockIcon />
-    },
-    {
-      name: 'Chat',
-      icon: <MessageIcon />
-    },
-    {
-      name: 'Profile',
-      icon: <ProfileIcon />
-    }
-  ]
-
   return (
-    <aside className='w-[32rem] flex flex-col gap-16 justify-center fixed left-48 top-1/2 -translate-y-1/2'>
-      <h2 className={`text-white text-3xl flex gap-3 items-center ${FONTS.POPPINS}`}>
-        <AppIcon />
+    <aside className='w-[32rem] flex flex-col gap-16 justify-center items-start fixed left-48 top-1/2 -translate-y-1/2'>
+      <h2 className={`text-white text-3xl flex gap-5 items-center ml-4 ${FONTS.POPPINS}`}>
+        <AppIcon width={40} height={40} />
         <div>
           <span className='font-semibold'>STUDY</span>
           <span className='italic'>MATE</span>
         </div>
       </h2>
-      <ul className='flex flex-col items-start gap-8'>
-        {paths.map(({ name, icon }) => (
-          <li key={name}>
-            <Link
-              href={`/${name.toLowerCase()}`}
-              className='flex gap-4 text-gray-10 button'
-              draggable={false}
-            >
-              <div className='*:size-8 *:stroke-[1.5px]'>{icon}</div>
-              <span className={`text-xl font-medium ${FONTS.POPPINS}`}>{name}</span>
-            </Link>
-          </li>
+
+      <ul className='flex flex-col items-start gap-4'>
+        {PATHS.map(({ name, icon }, i) => (
+          <RouteButton route={name} icon={icon} key={i} />
         ))}
       </ul>
     </aside>
   )
 }
+
+interface RouteButtonParams {
+  route: string
+  icon: JSX.Element
+}
+
+const RouteButton = ({ route, icon }: RouteButtonParams) => {
+  const [isSelected, setIsSelected] = useState(false)
+  const routeLowerCase = route.toLowerCase()
+
+  useEffect(() => {
+    const [, routeName] = location.pathname.split('/')
+    setIsSelected(routeName === routeLowerCase)
+  }, [location.pathname])
+
+  const style = isSelected ? 'text-white scale-[1.1] bg-gray-10/[.04]' : 'text-gray-10 button'
+
+  return (
+    <li>
+      <Link
+        href={`/${routeLowerCase}`}
+        className={`${style} px-6 py-3 rounded-full flex origin-left gap-3 relative items-center -translate-x-3 transition-all`}
+        draggable={false}
+      >
+        <div className='*:size-8 *:stroke-[1.5px]'>{icon}</div>
+        <span className={`text-xl font-medium ${FONTS.POPPINS}`}>{route}</span>
+      </Link>
+    </li>
+  )
+}
+
+const PATHS = [
+  {
+    name: 'Dashboard',
+    icon: <HomeIcon />
+  },
+  {
+    name: 'Chat',
+    icon: <MessageIcon />
+  },
+  {
+    name: 'Studyplan',
+    icon: <BookIcon />
+  },
+  {
+    name: 'Focus',
+    icon: <ClockIcon />
+  },
+  {
+    name: 'Profile',
+    icon: <ProfileIcon />
+  }
+]
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -93,12 +93,15 @@ export const ProfileIcon = () => (
   </svg>
 )
 
-export const AppIcon = (props: Props) => (
+export const AppIcon = ({
+  width = 32,
+  height = 32,
+  ...props
+}: { width?: number; height?: number } & Props) => (
   <Image
     src='/favicon.ico'
     alt='App logo'
-    width={32}
-    height={32}
+    {...{ width, height }}
     className='object-cover'
     {...props}
   />
@@ -518,5 +521,26 @@ export const CheckListIcon = (props: Props) => (
     <path d='M14 19l2 2l4 -4' />
     <path d='M9 8h4' />
     <path d='M9 12h2' />
+  </svg>
+)
+
+export const BookIcon = (props: Props) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='icon icon-tabler icons-tabler-outline icon-tabler-book-2'
+    {...props}
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M19 4v16h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12z' />
+    <path d='M19 16h-12a2 2 0 0 0 -2 2' />
+    <path d='M9 8h6' />
   </svg>
 )
