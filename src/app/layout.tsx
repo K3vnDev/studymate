345d0c7c36ec1c52816d6d615855c9fe736361ff
K3vnDev <158790560+K3vnDev@users.noreply.github.com>
2025-01-05 22:23:04 -0500

    Enchance App Background  component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { AppBackground } from '@/components/AppBackground'
+import { AppBackground, BlurredPoint } from '@/components/AppBackground'
 
 export const metadata: Metadata = {
   title: 'Studymate',
@@ -16,7 +16,11 @@ export default function RootLayout({
     <html lang='en'>
       <body className='min-h-screen py-6 px-48 grid overflow-y-scroll'>
         {children}
-        <AppBackground />
+
+        <AppBackground>
+          <BlurredPoint className='bg-[#6A71FC]/20' position='left-top' />
+          <BlurredPoint className='bg-[#6313ED]/15' position='right-bottom' />
+        </AppBackground>
       </body>
     </html>
   )
--- src/components/AppBackground.tsx
+++ src/components/AppBackground.tsx
@@ -1,14 +1,58 @@
-export const AppBackground = () => {
-  return (
-    <div className='bg-[#0C0C0C] fixed top-0 left-0 w-screen h-screen -z-10 pointer-events-none overflow-hidden'>
-      <BlurredPoint className='bg-[#6A71FC] top-0 left-0 opacity-[0.19]' />
-      <BlurredPoint className='bg-[#6313ED] top-[100vh] left-[100vw] opacity-[0.13]' />
-    </div>
-  )
+interface Props {
+  children: React.ReactNode
 }
 
-const BlurredPoint = ({ className = '' }) => (
-  <div
-    className={`absolute -translate-x-1/2 -translate-y-1/2 size-[53rem] blur-[200px] ${className}`}
-  />
+export const AppBackground = ({ children }: Props) => (
+  <div className='bg-[#0C0C0C] fixed top-0 left-0 w-screen h-screen -z-10 pointer-events-none overflow-hidden'>
+    {children}
+  </div>
 )
+
+interface BlurredPointProps {
+  position:
+    | 'left-top'
+    | 'center-top'
+    | 'right-top'
+    | 'left-center'
+    | 'center-center'
+    | 'right-center'
+    | 'left-bottom'
+    | 'center-bottom'
+    | 'right-bottom'
+
+  className?: string
+  margin?: number
+  size?: number
+}
+
+export const BlurredPoint = ({ className = '', margin = 5, size = 60, position }: BlurredPointProps) => {
+  const getPositionStyle = () => {
+    const [x, y] = position.split('-')
+
+    const getValue = (pos: string) => {
+      if (pos === 'center') {
+        return 50
+      }
+
+      const rawValue = ['top', 'left'].includes(pos) ? 0 : 100
+      return Math.abs(rawValue - margin)
+    }
+
+    return {
+      top: `${getValue(y)}vh`,
+      left: `${getValue(x)}vw`
+    }
+  }
+
+  const getSize = () => ({
+    width: `${size}rem`,
+    height: `${size}rem`
+  })
+
+  return (
+    <div
+      className={`${className} absolute -translate-x-1/2 -translate-y-1/2 blur-[200px]`}
+      style={{ ...getPositionStyle(), ...getSize() }}
+    />
+  )
+}
